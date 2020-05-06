82f9cbaaaeaba153a56ec16dda27ef596e161850
z0uh1r-dev <zouhir.amahzoune@gmail.com>
2020-05-06 16:49:33 +0000

    Updates

---
+++ precache-manifest.c19fd9afb0174e63e96eec0b8f1e05f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efc46cdfb0280f41e04e02e10e88a478",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "323cb39cacb4195f2430",
+    "url": "/monsters-rolodex/static/css/main.f884bda1.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "323cb39cacb4195f2430",
+    "url": "/monsters-rolodex/static/js/main.9035d83c.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
